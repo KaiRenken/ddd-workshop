5b786830dc0ebf27d16415eca926eacc658ba307
KaiRenken <kai.renken@gmx.de>
2024-12-02 06:57:18 +0100

    initial commit

---
+++ src/main/kotlin/de/neusta/dddworkshop/DddWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package de.neusta.dddworkshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddWorkshopApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddWorkshopApplication>(*args)
+}
---
+++ src/test/kotlin/de/neusta/dddworkshop/DddWorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.neusta.dddworkshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddWorkshopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
